0ab216a668e8b54214b139b363cd6f0eec054644
ijsathi <ijsathi2001rs@gmail.com>
2023-01-02 22:59:42 +0600

    property list dynamic done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
 import './Featured.css';
 
 const Featured = () => {
+    const { data, loading, error } = useFetch("/hotels/countByCity?cities=Dhaka,Dublin,London");
     return (
         <div className='featured'>
-            <div className="featuredItem">
-                <img className='featuredImg' src="https://cdn.pixabay.com/photo/2018/03/20/14/00/sea-3243357__480.jpg" alt="cox's bazar" />
-                <div className="featuredTitle">
-                    <h1>Cox's Bazar</h1>
-                    <h2>123 properties</h2>
+            {loading ? ("Loading please wait") : (<>
+                <div className="featuredItem">
+                    <img className='featuredImg' src="https://cdn.pixabay.com/photo/2018/03/20/14/00/sea-3243357__480.jpg" alt="cox's bazar" />
+                    <div className="featuredTitle">
+                        <h1>Dhaka</h1>
+                        <h2>{data[0]} properties</h2>
+                    </div>
                 </div>
-            </div>
-            <div className="featuredItem">
-                <img className='featuredImg' src="https://cdn.pixabay.com/photo/2018/03/20/14/00/sea-3243357__480.jpg" alt="cox's bazar" />
-                <div className="featuredTitle">
-                    <h1>Cox's Bazar</h1>
-                    <h2>121 properties</h2>
+                <div className="featuredItem">
+                    <img className='featuredImg' src="https://www.worldatlas.com/upload/65/4c/ce/shutterstock-1738979993.jpg" alt="dublin" />
+                    <div className="featuredTitle">
+                        <h1>Dublin</h1>
+                        <h2>{data[1]} properties</h2>
+                    </div>
                 </div>
-            </div>
-            <div className="featuredItem">
-                <img className='featuredImg' src="https://cdn.pixabay.com/photo/2018/03/20/14/00/sea-3243357__480.jpg" alt="cox's bazar" />
-                <div className="featuredTitle">
-                    <h1>Cox's Bazar</h1>
-                    <h2>123 properties</h2>
+                <div className="featuredItem">
+                    <img className='featuredImg' src="https://media.cntraveler.com/photos/57461927f086c628440a1369/5:4/w_1920,h_1536,c_limit/ann-and-roy-butler-trail-austin-alamy-EF3GKY.jpg" alt="austin" />
+                    <div className="featuredTitle">
+                        <h1>London</h1>
+                        <h2>{data[2]} properties</h2>
+                    </div>
                 </div>
-            </div>
+            </>)}
         </div>
     );
 };
--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,44 +1,31 @@
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
 import './propertyList.css';
 
 const PropertyList = () => {
+    const { data, loading, error } = useFetch("/hotels/countByType");
+
+    const images = [
+        "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        "https://www.gannett-cdn.com/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
+        "https://media.cntraveler.com/photos/53da5aa96dec627b149e509d/master/w_1024,h_768,c_limit/royal-ethiad-suite-jumeirah-abu-dhabi-2.jpg",
+        "https://content.r9cdn.net/res/images/horizon/ui/seoCommon/destinationtiles/seo_hotels.jpg?v=aeb8c67f83d5b9fd53ca97055fc8402800bf3ce4&cluster=4",
+        "https://www.xotels.com/wp-content/uploads/2020/03/hotel-room-type-xotels-hotel-management-company.webp"
+    ]
     return (
         <div className='pList'>
-            <div className="pListItem">
-                <img src="https://seoimgak.mmtcdn.com/blog/sites/default/files/images/The-Westin.jpg" alt="" className="pListImg" />
-                <div className="pLisTitles">
-                    <h1>Hotels</h1>
-                    <h2>233 hotels</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://seoimgak.mmtcdn.com/blog/sites/default/files/images/The-Westin.jpg" alt="" className="pListImg" />
-                <div className="pLisTitles">
-                    <h1>Hotels</h1>
-                    <h2>233 hotels</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://seoimgak.mmtcdn.com/blog/sites/default/files/images/The-Westin.jpg" alt="" className="pListImg" />
-                <div className="pLisTitles">
-                    <h1>Hotels</h1>
-                    <h2>233 hotels</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://seoimgak.mmtcdn.com/blog/sites/default/files/images/The-Westin.jpg" alt="" className="pListImg" />
-                <div className="pLisTitles">
-                    <h1>Hotels</h1>
-                    <h2>233 hotels</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://seoimgak.mmtcdn.com/blog/sites/default/files/images/The-Westin.jpg" alt="" className="pListImg" />
-                <div className="pLisTitles">
-                    <h1>Hotels</h1>
-                    <h2>233 hotels</h2>
-                </div>
-            </div>
+            {loading ? ("loading...") : (
+                <>
+                    {data &&
+                        images.map((img, i) => (
+                            <div key={i} className="pListItem">
+                                <img src={img} alt="" className="pListImg" />
+                                <div className="pLisTitles">
+                                    <h1>{data[i]?.type}</h1>
+                                    <h2>{data[i]?.count} {data[i]?.type}</h2>
+                                </div>
+                            </div>))}
+                </>)}
         </div>
     );
 };
